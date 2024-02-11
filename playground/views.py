0a1bc4cdecac5f49889ea0b5b5a683ad8f3a2660
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-02-11 14:36:54 +0530

    setting up views and routes

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# view function is a function that takes REQUEST ---> send RESPONSE
+# request : client's browser sends HTTP request to server, and server sends HTTP response to client
+# it is request handler
+#  it is also  called ACTION / VIEW
+
+
+def say_hello(request):
+    
+    # it is a function that takes a request and returns an HTTP response
+    # we can do anything like --- 1. pull data from database.
+    # 2. manipulate data / Transform
+    # 3. Send email
+
+    return HttpResponse("Hello World")
+
